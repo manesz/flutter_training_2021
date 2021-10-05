ed0087026c64636267dbe75f992ea29db5b14067
Lek CodeMobiles <chaiyasit.t@gmail.com>
2021-10-05 11:29:27 +0700

    1234

--- demo0/lib/src/services/network_service.dart
+++ demo0/lib/src/services/network_service.dart
@@ -1,6 +1,19 @@
 
+import 'dart:convert';
+
+import 'package:demo0/src/models/product.dart';
+
 class NetworkService {
   NetworkService._internal();
   static final NetworkService _instance = NetworkService._internal();
   factory NetworkService() => _instance;
+
+
+  Future<List<Product>> getProduct() async {
+    final response = await _dio.get("");
+    if (response.statusCode == 200) {
+      return productFromJson(jsonEncode(response.data));
+    }
+    throw Exception();
+  }
 }

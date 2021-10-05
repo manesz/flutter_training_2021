5ef116b9b0d58ee1157d94d80e3db34380989fbc
Lek CodeMobiles <chaiyasit.t@gmail.com>
2021-10-05 09:57:14 +0700

    1234

--- demo0/lib/src/app.dart
+++ demo0/lib/src/app.dart
@@ -1,6 +1,9 @@
 import 'package:demo0/src/configs/app_routes.dart';
 import 'package:demo0/src/pages/pages.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'constants/app_setting.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
@@ -10,7 +13,15 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: "Demo0",
       routes: AppRoute().getAll,
-      home: LoginPage()
+      home: _initialPage()
     );
   }
+}
+
+_initialPage() async {
+
+
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  final token = prefs.getString(AppSetting.token) ?? "";
+  return token.isEmpty ? LoginPage() : HomePage();
 }

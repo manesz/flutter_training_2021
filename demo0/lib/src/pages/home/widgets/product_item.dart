cfc82fbca6f4043268657d25b9f329414f44a0e2
Lek CodeMobiles <chaiyasit.t@gmail.com>
2021-10-05 13:31:00 +0700

    1234

--- demo0/lib/src/pages/home/widgets/product_item.dart
+++ demo0/lib/src/pages/home/widgets/product_item.dart
@@ -18,13 +18,32 @@ class ProductItem extends StatelessWidget {
   }
 
 
-  Positioned _buildOutOfStock() => Positioned(
+  Stack _buildImage(double maxHeight) {
+    final height = maxHeight * 0.65;
+    final image = product.image;
+    final stock = product.stock ?? 0;
+
+    return Stack(
+      children: [
+        SizedBox(
+          width: double.infinity,
+          height: height,
+          child: Container(color: Colors.red,),
+        ),
+        if (stock <= 0) _buildOutOfStock(),
+      ],
+    );
+  }
+
+
+
+  Positioned _buildOutOfStock() => const Positioned(
     top: 2,
     right: 2,
     child: Card(
       color: Colors.amber,
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+        padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
         child: Text(
           'out of stock',
           style: TextStyle(

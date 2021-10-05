e000a433bf1279843098897337fd34a76c6632dc
Lek CodeMobiles <chaiyasit.t@gmail.com>
2021-10-05 10:25:44 +0700

    1234

--- demo0/lib/src/pages/home/home_page.dart
+++ demo0/lib/src/pages/home/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:demo0/src/configs/app_routes.dart';
+import 'package:demo0/src/constants/app_setting.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,6 +16,17 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Home"),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              SharedPreferences prefs = await SharedPreferences.getInstance();
+              prefs.remove(AppSetting.token);
+              prefs.remove(AppSetting.username);
+              Navigator.pushReplacementNamed(context, AppRoute.login);
+            },
+            icon: const Icon(Icons.logout),
+          )
+        ],
       ),
       body: Container(),
     );

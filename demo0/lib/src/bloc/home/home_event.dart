dd500e9a2f3e79812e5e8429ed52cbd82d418c2b
Lek CodeMobiles <chaiyasit.t@gmail.com>
2021-10-06 11:09:16 +0700

    1234

--- demo0/lib/src/bloc/home/home_event.dart
+++ demo0/lib/src/bloc/home/home_event.dart
@@ -1,5 +1,12 @@
 part of 'home_bloc.dart';
 
 abstract class HomeEvent extends Equatable {
-  const HomeEvent();
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
+}
+
+class HomeEvent_Fetch extends HomeEvent{
+
 }

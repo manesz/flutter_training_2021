d28eaea3310b630f3d7287193f3a0c4c1a04d59e
Lek CodeMobiles <chaiyasit.t@gmail.com>
2021-10-06 09:35:18 +0700

    1234

--- demo0/lib/src/bloc/login/login_bloc.dart
+++ demo0/lib/src/bloc/login/login_bloc.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:demo0/src/models/user.dart';
 import 'package:equatable/equatable.dart';
 
 part 'login_event.dart';
--- demo0/lib/src/bloc/login/login_event.dart
+++ demo0/lib/src/bloc/login/login_event.dart
@@ -8,6 +8,21 @@ abstract class LoginEvent extends Equatable {
   List<Object?> get props => [];
 }
 
+// Login Event
 class LoginEvent_Login extends LoginEvent{
+  final User payload;
+  LoginEvent_Login(this.payload);
+}
 
+// Register Event
+class RegisterEvent_Login extends LoginEvent{
+  final User payload;
+  RegisterEvent_Login(this.payload);
+}
+
+
+// Forgot Event
+class ForgotEvent_Login extends LoginEvent{
+  final String username;
+  ForgotEvent_Login(this.username);
 }

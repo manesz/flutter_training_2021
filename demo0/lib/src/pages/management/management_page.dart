b2bca64d283a62362d1576f093063715acf02ced
Lek CodeMobiles <chaiyasit.t@gmail.com>
2021-10-06 13:46:52 +0700

    1234

--- demo0/lib/src/pages/management/management_page.dart
+++ demo0/lib/src/pages/management/management_page.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:demo0/src/models/product.dart';
 import 'package:flutter/material.dart';
 
 class ManagementPage extends StatefulWidget {
@@ -8,8 +11,19 @@ class ManagementPage extends StatefulWidget {
 }
 
 class _ManagementPageState extends State<ManagementPage> {
+  final _form = GlobalKey<FormState>();
+  var _product = Product(name: "", price: 0, stock: 0);
+  var _editMode = false;
+  File? _imageFile;
+
   @override
   Widget build(BuildContext context) {
+    final Object? arguments = ModalRoute.of(context)?.settings.arguments;
+    if (arguments != null && arguments is Product) {
+      _product = arguments;
+      _editMode = true;
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Management"),

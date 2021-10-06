2ee50798d3918db43e3c4e756c12c0839d72e804
Lek CodeMobiles <chaiyasit.t@gmail.com>
2021-10-06 10:20:01 +0700

    1234

--- demo0/lib/src/bloc/login/login_bloc.dart
+++ demo0/lib/src/bloc/login/login_bloc.dart
@@ -1,8 +1,13 @@
 import 'dart:async';
+import 'dart:js';
 
 import 'package:bloc/bloc.dart';
+import 'package:demo0/src/configs/app_routes.dart';
+import 'package:demo0/src/constants/app_setting.dart';
 import 'package:demo0/src/models/user.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 part 'login_event.dart';
 
@@ -13,7 +18,7 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
     on<LoginEvent>((event, emit) {
       if (event is LoginEvent_Login) {
         // Login
-        print("Login Bloc");
+        _doLogin(event.payload);
       } else if (event is LoginEvent_Register) {
         // Register
         print("Register Bloc");
@@ -22,5 +27,24 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
         print("Forgot Bloc");
       }
     });
+  }
+
+  Future<void> _doLogin(User payload) async {
+    final String username = payload.username;
+    final String password = payload.password;
+
+    if (username == 'admin' && password == 'password') {
+      // method 1
+      //Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> HomePage(),),);
+
+      // method 2
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setString(AppSetting.token, 'TExkgk0494oksrkf');
+      await prefs.setString(AppSetting.username, username);
+
+      Navigator.pushReplacementNamed(context, AppRoute.home);
+    } else {
+      print("Login failed");
+    }
   }
 }

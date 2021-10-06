8ef80f312ba60ba493425c599be2980afbeb1ca4
Lek CodeMobiles <chaiyasit.t@gmail.com>
2021-10-06 11:40:06 +0700

    1234

--- demo0/lib/src/pages/home/home_v2_page.dart
+++ demo0/lib/src/pages/home/home_v2_page.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:demo0/src/bloc/home/home_bloc.dart';
 import 'package:demo0/src/bloc/login/login_bloc.dart';
 import 'package:demo0/src/configs/app_routes.dart';
 import 'package:demo0/src/constants/app_setting.dart';
@@ -19,10 +20,18 @@ class HomePageV2 extends StatefulWidget {
 class _HomePageV2State extends State<HomePageV2> {
   final _refreshController = StreamController<void>();
   var _isGrid = false;
+  
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print("1234..");
+    context.read<HomeBloc>().add(HomeEvent_Fetch());
+  }
 
   @override
   Widget build(BuildContext context) {
-    List<String> dummy = ["Angular", "React", "Flutter"];
+    final homeState = context.watch<HomeBloc>().state;
 
     return Scaffold(
       backgroundColor: Colors.grey[200],
@@ -30,12 +39,12 @@ class _HomePageV2State extends State<HomePageV2> {
         title: Text("Home BlocV2"),
         actions: [
           IconButton(
-            onPressed: (){
+            onPressed: () {
               setState(() {
                 _isGrid = !_isGrid;
               });
             },
-            icon: _isGrid ? const Icon(Icons.grid_3x3) : const Icon(Icons.list) ,
+            icon: _isGrid ? const Icon(Icons.grid_3x3) : const Icon(Icons.list),
           ),
           IconButton(
             onPressed: () => context.read<LoginBloc>().add(LoginEvent_Logout()),
@@ -43,38 +52,27 @@ class _HomePageV2State extends State<HomePageV2> {
           )
         ],
       ),
-      body: StreamBuilder(
-        stream: _refreshController.stream,
-        builder: (context, snapshot) {
-          return FutureBuilder<List<Product>>(
-            future: NetworkService().getProduct(),
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) {
-                return Container(color: Colors.white);
-              }
-
-              final products = snapshot.data ?? [];
-              return _isGrid ? _buildGridView(products) : _buildListView(products);
-            },
-          );
-        },
-      ),
+      body: _isGrid
+          ? _buildGridView(homeState.products)
+          : _buildListView(homeState.products),
     );
   }
 
   Widget _buildListView(List<Product> products) {
     return RefreshIndicator(
-      onRefresh:  ()async => _refreshController.sink.add(null),
+      onRefresh: () async => _refreshController.sink.add(null),
       child: ListView.builder(
-        padding: const EdgeInsets.only(top:10),
+        padding: const EdgeInsets.only(top: 10),
         itemBuilder: (context, index) {
           return Padding(
             padding: const EdgeInsets.only(left: 8, right: 8),
             child: SizedBox(
-              child: ProductItem(product: products[index],
-                onTap: (){
+              child: ProductItem(
+                product: products[index],
+                onTap: () {
                   print(products[index].name);
-                },),
+                },
+              ),
               height: 350,
             ),
           );
@@ -86,21 +84,24 @@ class _HomePageV2State extends State<HomePageV2> {
 
   Widget _buildGridView(List<Product> products) {
     return RefreshIndicator(
-      onRefresh:  ()async => _refreshController.sink.add(null),
+      onRefresh: () async => _refreshController.sink.add(null),
       child: GridView.builder(
-        padding: const EdgeInsets.only(top:10),
+        padding: const EdgeInsets.only(top: 10),
         itemBuilder: (context, index) {
-          return ProductItem(product: products[index],
-            onTap: (){
+          return ProductItem(
+            product: products[index],
+            onTap: () {
               print(products[index].name);
-            },);
+            },
+          );
         },
         itemCount: products.length,
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           crossAxisSpacing: 1,
           mainAxisSpacing: 1,
-          childAspectRatio: 0.76, // set height ratio -  (itemWidth / itemHeight)
+          childAspectRatio:
+              0.76, // set height ratio -  (itemWidth / itemHeight)
         ),
       ),
     );

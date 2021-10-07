9330f1574aa1599446e9ee48f5e7d94f7eccc29d
Lek CodeMobiles <chaiyasit.t@gmail.com>
2021-10-07 15:55:26 +0700

    1234

--- demo0/webapi/server.js
+++ demo0/webapi/server.js
@@ -1,1 +1,10 @@
-console.log("1234")
+const express = require('express')
+const app = express()
+
+app.get("/", (req, res)=>{
+    res.json({result: "ok"})
+})
+
+app.listen(3000,()=>{
+    console.log("Server is running...")
+});

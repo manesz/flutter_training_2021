dbaf465caede050fb5d7f94894d0eafede740c59
Lek CodeMobiles <chaiyasit.t@gmail.com>
2021-10-07 10:19:20 +0700

    1234

--- demo0/lib/src/bloc/management/management_bloc.dart
+++ demo0/lib/src/bloc/management/management_bloc.dart
@@ -6,12 +6,15 @@ import 'package:demo0/src/models/product.dart';
 import 'package:equatable/equatable.dart';
 
 part 'management_event.dart';
+
 part 'management_state.dart';
 
 class ManagementBloc extends Bloc<ManagementEvent, ManagementState> {
-  ManagementBloc() : super(ManagementInitial()) {
-    on<ManagementEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  ManagementBloc() : super(ManagementState()) {
+    on<ManagementEvent_Submit>(mapStateToManagementEvent_Submit);
+  }
+
+  mapStateToManagementEvent_Submit(event, emit) {
+
   }
 }

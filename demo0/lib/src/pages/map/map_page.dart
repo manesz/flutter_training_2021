8cdae18530bd973a89ea4809347f62ef9d14c45a
Lek CodeMobiles <chaiyasit.t@gmail.com>
2021-10-07 13:39:00 +0700

    1234

--- demo0/lib/src/pages/map/map_page.dart
+++ demo0/lib/src/pages/map/map_page.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({Key? key}) : super(key: key);
@@ -8,11 +11,30 @@ class MapPage extends StatefulWidget {
   _MapPageState createState() => _MapPageState();
 }
 
+// 13.7462463,100.5325515,17z
 class _MapPageState extends State<MapPage> {
   static const CameraPosition _initMap = CameraPosition(
-    target: LatLng(13.7465354, 100.532752),
-    zoom: 12,
+    target: LatLng(13.7462463, 100.5325515),
+    zoom: 20,
   );
+
+  StreamSubscription<LocationData>? _locationSubscription;
+  final _locationService = Location();
+  final Set<Marker> _markers = {};
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _markers.add(Marker(
+      // important. unique id
+      markerId: MarkerId("1"),
+      position: LatLng(13.7462463, 100.5325515),
+      onTap: () async {
+        //todo
+      },
+    ));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +43,8 @@ class _MapPageState extends State<MapPage> {
         title: Text("Map"),
       ),
       body: GoogleMap(
-        mapType: MapType.normal,
+        markers: _markers,
+        mapType: MapType.hybrid,
         initialCameraPosition: _initMap,
       ),
     );

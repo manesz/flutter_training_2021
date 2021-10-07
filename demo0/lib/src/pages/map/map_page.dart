9992bfe1b6b789c2b382020edbc9bfcbf714efc5
Lek CodeMobiles <chaiyasit.t@gmail.com>
2021-10-07 14:15:22 +0700

    1234

--- demo0/lib/src/pages/map/map_page.dart
+++ demo0/lib/src/pages/map/map_page.dart
@@ -76,11 +76,30 @@ class _MapPageState extends State<MapPage> {
       );
     }
 
-    // _controller.future.then((controller) => controller.moveCamera(
-    //     CameraUpdate.newLatLngBounds(_boundsFromLatLngList(data), 32)));
+    _controller.future.then((controller) => controller.moveCamera(
+        CameraUpdate.newLatLngBounds(_boundsFromLatLngList(data), 32)));
     setState(() {});
   }
 
+
+  LatLngBounds _boundsFromLatLngList(List<LatLng> list) {
+    double? x0, x1, y0, y1 = 0;
+    for (LatLng latLng in list) {
+      if (x0 == null) {
+        x0 = x1 = latLng.latitude;
+        y0 = y1 = latLng.longitude;
+      } else {
+        if (latLng.latitude > x1!) x1 = latLng.latitude;
+        if (latLng.latitude < x0) x0 = latLng.latitude;
+        if (latLng.longitude > y1!) y1 = latLng.longitude;
+        if (latLng.longitude < y0!) y0 = latLng.longitude;
+      }
+    }
+    return LatLngBounds(
+      northeast: LatLng(x1!, y1!),
+      southwest: LatLng(x0!, y0!),
+    );
+  }
 
   Future<void> _addMarker(
       LatLng position, {
